59108fff99bb42578a5f4e8d5e714d54f280d111
muralichowhaniiit <muralichowhaniiit@gmail.com>
2021-11-19 19:13:58 +0530

    Eazy_rooms mobile screens

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes/AppRoutes";
+import Landingpage from "./components/home/homelandingpage/Landingpage";
+import Notification from "./components/notifications/Notification";
+import SearchBar from "./components/search/SearchBar";
+import ProfileMenu from "./components/profilemenu/ProfileMenu";
+import HouseKeeping from "./components/home/homelandingpage/HouseKeeping";
+import Reception from "./components/home/homelandingpage/Reception";
+import Accessories from "./components/home/homelandingpage/Accessories";
+import Requests from "./components/home/homelandingpage/Requests";
+import SpaAndSlon from "./components/home/homelandingpage/SpaAndSalon";
+import RequestPlaced from "./components/home/RequestPlaced";
+import ReportIssue from "./components/home/reportissues/Reportissue";
+import Chat from "./components/home/chat/Chat";
+import Header from "./components/header/header";
+import ChatConsole from "./components/home/chat/ChatConsole";
+import RoomDining from "./components/home/roomdining/RoomDining";
+import RequestModals from "./components/modals/Modals";
+import ModifyProfile from "./components/profilemenu/ModifyProfile";
+import Store from "./components/productstore/Store";
+import Product from "./components/productstore/Product";
+import OrderAndRequests from "./components/ordersandrequests/OrderAndRequests";
+import Cart from "./components/cart/Cart";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-sm-12 col-md-4 col-lg-4"></div>
+        <div className="col-sm-12 col-md-4 col-lg-4">
+          <div className="border custom-rounded"
+            style={{ height: "100%", background: "#fff", height: "530px", overflowX: "hidden", maxWidth: "100%", overflowX: "hidden", borderRadius:"20px" }}
+          >
+            {/* <MainLogo /> */}
+            <Router>
+              <AppRoutes />
+            </Router>
+            <div className="p3">
+              {/* <SearchBar /> */}
+              {/* <Landingpage /> */}
+              {/* <Notification /> */}
+              {/* <ProfileMenu /> */}
+              {/* <HouseKeeping /> */}
+              {/* <Reception /> */}
+              {/* <Accessories /> */}
+              {/* <Requests /> */}
+              {/* <SpaAndSlon /> */}
+              {/* <RequestPlaced /> */}
+              {/* <ReportIssue /> */}
+              {/* <Chat /> */}
+              {/* <Header /> */}
+              {/* <ChatConsole /> */}
+              {/* <RoomDining /> */}
+              {/* <RequestModals /> */}
+              {/* <ModifyProfile /> */}
+              {/* <Store /> */}
+              {/* <Product /> */}
+              {/* <OrderAndRequests /> */}
+              {/* <Cart /> */}
+              {/* <RequestModals /> */}
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-12 col-md-4 col-lg-4"></div>
+      </div>
     </div>
   );
 }
